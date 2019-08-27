2912ee05a393dc5184dd5ff501a51616b3c87944
Bobby Kozora <robert.kozora@skf.com>
2019-08-27 15:42:30 -0400

    modifications to inject working dockerfile

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-'use strict';
+'use strict'
 
-const BbPromise = require('bluebird');
-const fse = require('fs-extra');
-const generateCoreTemplate = require('./lib/generateCoreTemplate');
-const ecr = require('./lib/ecr');
-const docker = require('./lib/docker');
-const batchenvironment = require('./lib/batchenvironment');
-const batchtask = require('./lib/batchtask');
-const awscli = require('./lib/awscli');
-const _ = require('lodash');
+const BbPromise = require('bluebird')
+const fse = require('fs-extra')
+const generateCoreTemplate = require('./lib/generateCoreTemplate')
+const ecr = require('./lib/ecr')
+const docker = require('./lib/docker')
+const batchenvironment = require('./lib/batchenvironment')
+const batchtask = require('./lib/batchtask')
+const awscli = require('./lib/awscli')
+const _ = require('lodash')
 
-BbPromise.promisifyAll(fse);
+BbPromise.promisifyAll(fse)
 
 class ServerlessAWSBatch {
-  constructor(serverless, options) {
-    this.serverless = serverless;
-    this.options = options;
-    this.provider = this.serverless.getProvider('aws');
+  constructor (serverless, options) {
+    this.serverless = serverless
+    this.options = options
+    this.provider = this.serverless.getProvider('aws')
 
     // Make sure that we add the names for our ECR, docker, and batch resources to the provider
     _.merge(
       this.provider.naming,
       {
-        'getECRLogicalId': ecr.getECRLogicalId,
-        'getECRRepositoryName': ecr.getECRRepositoryName,
-        'getECRRepositoryURL': ecr.getECRRepositoryURL,
-        'getDockerImageName': docker.getDockerImageName,
-        'getBatchServiceRoleLogicalId': batchenvironment.getBatchServiceRoleLogicalId,
-        'getBatchInstanceManagementRoleLogicalId': batchenvironment.getBatchInstanceManagementRoleLogicalId,
-        'getBatchInstanceManagementProfileLogicalId': batchenvironment.getBatchInstanceManagementProfileLogicalId,
-        'getBatchSpotFleetManagementRoleLogicalId': batchenvironment.getBatchSpotFleetManagementRoleLogicalId,
-        'getBatchJobExecutionRoleLogicalId': batchenvironment.getBatchJobExecutionRoleLogicalId,
-        'getLambdaScheduleExecutionRoleLogicalId': batchenvironment.getLambdaScheduleExecutionRoleLogicalId,
-        'getBatchComputeEnvironmentLogicalId': batchenvironment.getBatchComputeEnvironmentLogicalId,
-        'getBatchJobQueueLogicalId': batchenvironment.getBatchJobQueueLogicalId,
-        'getBatchJobQueueName': batchenvironment.getBatchJobQueueName,
-        'getJobDefinitionLogicalId': batchtask.getJobDefinitionLogicalId
+        getECRLogicalId: ecr.getECRLogicalId,
+        getECRRepositoryName: ecr.getECRRepositoryName,
+        getECRRepositoryURL: ecr.getECRRepositoryURL,
+        getDockerImageName: docker.getDockerImageName,
+        getBatchServiceRoleLogicalId: batchenvironment.getBatchServiceRoleLogicalId,
+        getBatchInstanceManagementRoleLogicalId: batchenvironment.getBatchInstanceManagementRoleLogicalId,
+        getBatchInstanceManagementProfileLogicalId: batchenvironment.getBatchInstanceManagementProfileLogicalId,
+        getBatchSpotFleetManagementRoleLogicalId: batchenvironment.getBatchSpotFleetManagementRoleLogicalId,
+        getBatchJobExecutionRoleLogicalId: batchenvironment.getBatchJobExecutionRoleLogicalId,
+        getLambdaScheduleExecutionRoleLogicalId: batchenvironment.getLambdaScheduleExecutionRoleLogicalId,
+        getBatchComputeEnvironmentLogicalId: batchenvironment.getBatchComputeEnvironmentLogicalId,
+        getBatchJobQueueLogicalId: batchenvironment.getBatchJobQueueLogicalId,
+        getBatchJobQueueName: batchenvironment.getBatchJobQueueName,
+        getJobDefinitionLogicalId: batchtask.getJobDefinitionLogicalId
       }
-    );
+    )
 
     // Define inner lifecycles
     this.commands = {}
@@ -52,7 +52,7 @@ class ServerlessAWSBatch {
         .then(batchtask.compileBatchTasks),
 
       'after:package:createDeploymentArtifacts': () => BbPromise.bind(this)
-        .then(docker.buildDockerImage),
+        .then(docker.buildDockerImageFromDockerfile),
 
       'before:aws:deploy:deploy:uploadArtifacts': () => BbPromise.bind(this)
         .then(docker.pushDockerImageToECR),
@@ -63,4 +63,4 @@ class ServerlessAWSBatch {
   }
 }
 
-module.exports = ServerlessAWSBatch;
+module.exports = ServerlessAWSBatch
